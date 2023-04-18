6018e4fc0cf529fe875865b127aa3a71ec2587c2
Aman Kumar <amankumar15m01.gmail.com>
2023-04-19 01:08:10 +0530

    update

--- routes/dataRoute.js
+++ routes/dataRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Data = require("../models/dataModel");
 
-router.post('/createData', async (req, res) => {
+router.post('/', async (req, res) => {
     const newData = new Data({ user: req.body.user, typeOfData: req.body.type, data: req.body.data });
     try {
         const result = await newData.save();
@@ -12,7 +12,7 @@ router.post('/createData', async (req, res) => {
     }
 });
 
-router.get('/', async (req, res) => { 
+router.get('/', async (req, res) => {
     try {
         const data = await Data.find();
         res.send(data);
@@ -20,18 +20,24 @@ router.get('/', async (req, res) => {
         return res.status(400).json({ error });
     }
 });
-    
-router.post('/dataById', async (req, res) => {
-    const dataId=req.body.dataId;
-    try {
-        // console.log("this is findone:" , dataId);
 
+router.post('/dataById', async (req, res) => {
+    const dataId = req.body.dataId;
+    try {
         const data = await Data.findById(dataId);
-        // console.log("data after findById: ", data);
         res.send(data);
     } catch (error) {
         return res.status(400).json({ error });
     }
 });
 
-module.exports = router;
+router.delete("/delete/:id", async (req, res) => {
+    try {
+        const result = await Data.deleteOne({ _id: req.params.id });
+        res.send(result);
+    } catch (error) {
+        return res.status(400).json({ error });
+    }
+});
+
+module.exports = router;  

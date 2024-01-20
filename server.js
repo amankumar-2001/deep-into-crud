817803a7ec7622a3647b408131980f4dafcf63a3
Aman Kumar <apcode15@gmail.com>
2024-01-20 17:12:45 +0530

    fix: vercel deployment

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const routes = require("./routes");
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 
-// app.use("/driveDIC", routes);
+app.use("/driveDIC", routes);
 
 app.get("/", (req, res) => {
   res.send(`I'm ON...`);
